0afeba849423e840848ba61f06d007fc94b26f3e
Anderis <88600844+Anderis@users.noreply.github.com>
2023-08-29 05:42:21 -0500

    Upload
    Post

---
+++ launch.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import t3c
+import t3cResponse as responses
+from dotenv import load_dotenv
+import os
+
+if __name__ == '__main__':
+    load_dotenv()  # Load environment variables from .env file
+    mochiResponses = responses  # Pass the responses module to the bot module
+    bot_token = os.getenv('DISCORD_TOKEN')  # Retrieve bot token from environment variables
+    t3c.run_discord_bot(bot_token)
---
+++ t3cResponse.py
@@ -0,0 +1,4 @@
+import random
+
+def get_response(message: str) -> str:
+    message = message.lower()
