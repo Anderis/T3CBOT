3b0646bb9e58c3b2a8dde6d76418e5028129f647
Anderis <88600844+Anderis@users.noreply.github.com>
2023-09-01 14:56:25 -0500

    Custom Commands Import
    Bot Launch Wrapper Completion.

--- bot.py
+++ bot.py
@@ -1,39 +1,46 @@
 import discord
 import responses
-from dotenv import load_dotenv
 import os
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()  # Load environment variables from .env file
+TOKEN = os.getenv("DISCORD_TOKEN")  # Token grabber
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)  # Initialize commands.Bot with intents
+
+@bot.command(name='test')
+async def test_command(ctx):
+    await ctx.send("This is a test")
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
-
     except Exception as e:
         print(e)
 
-def run_discord_bot():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is now running!')
 
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
 
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
+    print(f'{username} said: "{user_message}" in ({channel})')
+    if user_message[0] == '?':
+        user_message = user_message[1:]
+        await send_message(message, user_message, is_private=True)
+    else:
+        await bot.process_commands(message)  # Handle bot commands
 
-        print(f'{username} said: "{user_message}" in ({channel})')
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-    client.run(TOKEN)
+bot.run(TOKEN)
