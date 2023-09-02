971b17183c56184d15ac1cb5f6d6e141b80b4c03
Anderis <88600844+Anderis@users.noreply.github.com>
2023-09-02 00:30:33 -0500

    Hero's Command Wrappers
    Added all heroes with current placeholders

--- bot.py
+++ bot.py
@@ -16,24 +16,65 @@ bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)  
 async def test_command(ctx):
     await ctx.send("This is a test")
 
-
 @bot.command(name='info')
-async def info_command(ctx):
+async def info_command(ctx, hero_name: str = None):
+    if hero_name:
+        hero_name = hero_name.lower()  # Convert input to lowercase
 
-    if ctx.message.content[6:].lower() == 'quincy':
-        await ctx.send("This is information")
-    elif ctx.message.content[6:].lower() == 'obyn':
-        await ctx.send("This is information")
-    elif ctx.message.content[6:].lower() == 'gwen':
-        await ctx.send("This is information")
-    elif ctx.message.content[6:].lower() == 'pat':
-        await ctx.send("This is information information")
-    elif ctx.message.content[6:].lower() == 'ben':
-        await ctx.send("This is information information")
-    elif ctx.message.content[6:].lower() == 'sauda':
-        await ctx.send("This is information information")
+        # Define hero aliases
+        hero_aliases = {
+            'quincy': ['quinc', 'quin', 'q'],
+            'obyn': ['oby', 'o'],
+            'gwen': ['g', 'gwendolyn'],
+            'pat': ['fusty', 'pat fusty', 'p', 'pf'],
+            'ben': ['benjamin', 'benjy', 'benj', 'be', 'benny'],
+            'sauda': ['s'],
+            'brickell': ['brick', 'admiral', 'admiral brickell', 'ab', 'adm'],
+            'adora': ['dora', 'ado'],
+            'churchill': ['captain', 'captain churchill', 'c'],
+            'ezili': ['ez'],
+            'ettiene': ['etn', 'et'],
+            'jones': ['striker', 'striker jones', 'sj', 'st', 'j']
+        }
+
+        # Check for heroes and aliases
+        for hero, aliases in hero_aliases.items():
+            if hero_name == hero or hero_name in aliases:
+                # Add specific information for each hero/alias
+                if hero == 'quincy':
+                    await ctx.send("This is information about Quincy.")
+                elif hero == 'obyn':
+                    await ctx.send("This is information about Obyn.")
+                elif hero == 'gwen':
+                    await ctx.send("This is information about Gwen.")
+                elif hero == 'pat':
+                    await ctx.send("This is information about Pat.")
+                elif hero == 'ben':
+                    await ctx.send("This is information about Ben.")
+                elif hero == 'sauda':
+                    await ctx.send("This is information about Sauda.")
+                elif hero == 'brickell':
+                    await ctx.send("This is information about Brickell.")
+                elif hero == 'adora':
+                    await ctx.send("This is information about Adora.")
+                elif hero == 'churchill':
+                    await ctx.send("This is information about Churchill.")
+                elif hero == 'ezili':
+                    await ctx.send("This is information about Ezili.")
+                elif hero == 'ettiene':
+                    await ctx.send("This is information about Ettiene.")
+                elif hero == 'jones':
+                    await ctx.send("This is information about Jones.")
+                return  # Exit the loop if a match is found
+
+        # If no match found
+        await ctx.send("I don't have information about that hero.")
     else:
-        await ctx.send("Please type in a hero's name after **Info** for example `!info Obyn`")
+        await ctx.send("Please type in a hero's name after **info**. For example: `!info Obyn`")
+
+
+
+
 async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
